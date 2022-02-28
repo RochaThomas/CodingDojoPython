e0ff9df6a342dd8ee4b3b6b7c61935685795dc76
Tommy Rocha <tomrocha99@gmail.com>
2022-02-28 00:46:31 -0800

    hangry login and reg template commit

---
+++ project_week/hangry/flask_app/static/script.js
@@ -0,0 +1,42 @@
+
+function nav_login_react(element) {
+    element.classList.add("on_hover_navbar_login");
+}
+function nav_login_unreact(element) {
+    element.classList.remove("on_hover_navbar_login");
+}
+
+function nav_sign_up_react(element) {
+    element.classList.add("on_hover_navbar_sign_up");
+}
+function nav_sign_up_unreact(element) {
+    element.classList.remove("on_hover_navbar_sign_up");
+}
+
+function box_react(element) {
+    element.classList.add("on_hover_boxes");
+}
+function box_unreact(element) {
+    element.classList.remove("on_hover_boxes");
+}
+
+function box_button_react(element) {
+    element.classList.add("on_hover_boxes_buttons");
+}
+function box_button_unreact(element) {
+    element.classList.remove("on_hover_boxes_buttons");
+}
+
+function sign_up_react(element) {
+    element.classList.add("on_hover_sign_up_button");
+}
+function sign_up_unreact(element) {
+    element.classList.remove("on_hover_sign_up_button");
+}
+
+function login_react(element) {
+    element.classList.add("on_hover_login_button");
+}
+function login_unreact(element) {
+    element.classList.remove("on_hover_login_button");
+}
